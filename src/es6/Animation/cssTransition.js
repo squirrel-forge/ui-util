c5380b5ce2d5322721b4671acef565681ee678d4
dasiux <me@siux.info>
2022-12-08 13:52:54 +0100

    improve cssTransition handler

--- src/es6/Animation/cssTransition.js
+++ src/es6/Animation/cssTransition.js
@@ -1,16 +1,22 @@
 /**
+ * Requires
+ */
+import { afterPaint } from './afterPaint.js';
+
+/**
  * Css transition handler
  * @param {HTMLElement} element - Target element
  * @param {null|Function} initial - Initial state
  * @param {null|Function} target - Target state
+ * @param {null|Function} ended - Ended state
  * @param {null|Function} complete - Complete state
- * @param {number} delay - Delay in ms
  * @return {void}
  */
-export function cssTransition( element, initial = null, target = null, complete = null, delay = 10 ) {
+export function cssTransition( element, initial = null, target = null, ended = null, complete = null ) {
     if ( initial ) initial( element );
     element.addEventListener( 'transitionend', () => {
-        window.setTimeout( () => { if ( complete ) complete( element ); }, delay );
+        if ( ended ) ended( element );
+        afterPaint( () => { if ( complete ) complete( element ); } );
     }, { once : true } );
-    window.setTimeout( () => { if ( target ) target( element ); }, delay );
+    afterPaint( () => { if ( target ) target( element ); } );
 }
