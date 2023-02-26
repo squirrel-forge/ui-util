06ef414ea69fdc0cd4aa01fad29b624ad37bc8e9
dasiux <me@siux.info>
2023-02-26 03:43:15 +0100

    improve bindNodeList

--- src/es6/Events/bindNodeList.js
+++ src/es6/Events/bindNodeList.js
@@ -5,18 +5,17 @@
  * @param {boolean} unbind - Remove event listeners
  * @return {void}
  */
-export function bindNodeList( elements, events, unbind ) {
-    unbind = !!unbind;
+export function bindNodeList( elements, events, unbind = false ) {
     if ( !( elements instanceof Array || elements instanceof NodeList ) ) {
-        throw new Error( 'bindNodeList() Argument elements must be a NodeList or an Array' );
+        throw new Error( 'bindNodeList() First argument elements must be a NodeList or an Array of HTMLElement' );
     }
     if ( !( events instanceof Array ) ) {
-        throw new Error( 'bindNodeList() Argument events must be an Array' );
+        throw new Error( 'bindNodeList() Second argument events must be an Array of addEventListener arguments' );
     }
     for ( let i = 0; i < elements.length; i++ ) {
         for ( let j = 0; j < events.length; j++ ) {
-            if ( !( events[ j ] instanceof Array ) ) {
-                throw new Error( 'bindNodeList() Argument events array must only contain addEventListener arguments as Arrays' );
+            if ( !( events[ j ] instanceof Array && events[ j ].length > 1 ) ) {
+                throw new Error( 'bindNodeList() Second argument events array must only contain Arrays of addEventListener arguments' );
             }
             elements[ i ][ ( unbind ? 'remove' : 'add' ) + 'EventListener' ]( ...events[ j ] );
         }
