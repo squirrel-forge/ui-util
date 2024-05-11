05bdefa31efc625c40d27df0301661975a3b23b6
dasiux <me@siux.info>
2024-05-11 16:52:15 +0200

    custom value conversion

--- src/es6/String/simpleReplace.js
+++ src/es6/String/simpleReplace.js
@@ -4,13 +4,15 @@
  * @param {Object} data - Object with replace vars
  * @param {null|string} prefix - Regex prefix
  * @param {null|string} suffix - Regex suffix
+ * @param {null|Function} converter - Convert value before replacement
  * @return {string} - Rendered template string
  */
-export function simpleReplace( tmpl, data, prefix = ':', suffix = '' ) {
+export function simpleReplace( tmpl, data, prefix = ':', suffix = '', converter = null ) {
     const keys = Object.keys( data );
     let regex, value;
     for ( let key = 0; key < keys.length; key++ ) {
         value = data[ keys[ key ] ];
+        if ( converter ) value = converter( value, keys[ key ] );
         regex = new RegExp( prefix + keys[ key ] + suffix, 'g' );
         tmpl = tmpl.replace( regex, value );
     }
