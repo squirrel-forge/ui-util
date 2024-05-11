4f12b8ce3d4932cd9fc243f92cd132720c2e305e
dasiux <me@siux.info>
2024-05-11 16:49:16 +0200

    variable arguments + cancellable

--- src/es6/Events/debounce.js
+++ src/es6/Events/debounce.js
@@ -8,16 +8,19 @@
  * Debounce event
  * @param {Function|DebounceEventHandler} func - Event handler
  * @param {null|number} delay - Debounce delay, default: 350
+ * @param {null|Function} always - Always run callback
  * @return {function(*=): void} - Wrapped callback
  */
-export function debounce( func, delay = null ) {
+export function debounce( func, delay = null, always = null ) {
     delay = typeof delay === 'number' ? delay : 350;
     let timer = 0;
-    return function( event ) {
+    return function( ...params ) {
+        let cancel = false;
+        if ( always ) cancel = always( ...params );
         if ( timer ) {
             window.clearTimeout( timer );
             timer = 0;
         }
-        timer = window.setTimeout( func, delay, event );
+        if ( !cancel ) timer = window.setTimeout( func, delay, ...params );
     };
 }
