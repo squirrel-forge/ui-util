4bc15c0565da3e8be4d343cb48b2f148d1077612
dasiux <me@siux.info>
2024-05-20 16:00:56 +0200

    Create orientation.js

---
+++ src/js/Number/orientation.js
@@ -0,0 +1,9 @@
+/**
+ * Get orientation
+ * @param {number} width - Width
+ * @param {number} height - Height
+ * @return {('square'|'landscape'|'portrait')} - Orientation string
+ */
+export function orientation( width, height ) {
+    return width === height ? 'square' : ( width > height ? 'landscape' : 'portrait' );
+}
